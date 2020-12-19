334256fd790c19b3a6a40c717b37e346c5bff251
Banser2 <banser2009@gmail.com>
2020-12-18 23:01:03 -0300

    Aulajavascriptes6

---
+++ main.js
@@ -0,0 +1,64 @@
+function botao(){
+    document.getElementById("Agradecimento").innerHTML = "Agradecimento";
+    //console.log(document.getElementById("Agradecimento"));
+    //alert("Obrigado Por Clicar");
+}
+
+function redirecionar(){
+    window.open("https://github.com/Banser2/Aulajavascript");
+}
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+/*
+function validaidade(idade){
+    var validaidade;
+    if (idade >=18){
+        validaidade = true
+
+    }else{
+        validaidade = false
+    }
+    return validaidade;
+}
+var idade = prompt("Qula é sua Idade");
+console.log(validaidade(idade));
+
+//alert(soma (5,10));
+*/
+
+/*var d = new Date();
+alert(d);
+*/
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1;
+};
+*/
+
+
+/*var idade = prompt("Qual é Sua Idade");
+if (idade >= 18){
+    alert("Maior de Idade");
+}else{
+    alert("Menor de Idade");
+};
+*/
+
+// fruta = {nome: "maça", cor: "Vermelha"}
+//console.log(fruta);
+/*
+var nome = "Sergio bandeira";
+var idade = 30;
+var idade2 = 29;
+alert(idade2 + idade + " anos");
+*/
+
+//var lista = ["pera", " maça", "jaca", "laranja"];
+/*.push("uva");
+lista.pop("laranja");*/
+//console.log(lista);
+//console.log(lista.toString())
+//cosole.log(lista.join(" "))
---
+++ tiposvariaveis.js
@@ -0,0 +1,22 @@
+//retorna o tamanho da string
+const textSize = "Texto".length;
+console.log('Quantidade de letras; ${textSize}');
+
+
+//transforma numero para string
+const numberAsString = myNumber.tostring();
+console.log('numero tranformado em string: ', typeof numberAsstring);
+
+
+
+
+
+//retorna numero com um numero de casa decimais
+const fixedTwoDecimais = myNumber.tofixed(2);
+console.log('\Numero com duas casas decimais:', fixedTwoDecimais);
+
+//transforma um string em float
+console.log ('\nString paseada para float ', parseFloat('13.22'));
+
+//Transfoma uma string em int 
+console.log('\nstring parseada para int', parseint('13.20'));
